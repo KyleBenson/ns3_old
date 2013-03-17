5fa5869097a387eaaca255a0c0ee47feeca4c23b
Kyle Benson <kyle.edward.benson@gmail.com>
2013-03-16 17:23:46 -0700

    Making module for geocron.

---
+++ src/geocron/helper/geocron-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __GEOCRON_HELPER_H__
+#define __GEOCRON_HELPER_H__
+
+#include "ns3/geocron.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __GEOCRON_HELPER_H__ */
+
---
+++ src/geocron/model/geocron.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef __GEOCRON_H__
+#define __GEOCRON_H__
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* __GEOCRON_H__ */
+
