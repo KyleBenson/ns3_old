59ebe832676ed019d493dd413c286617cb074e55
Kyle Benson <kebenson@ics.uci.edu>
2013-04-25 19:46:17 -0700

    added cloning repo into ns-3 source before building

--- install-ns3.sh
+++ install-ns3.sh
@@ -1,11 +1,31 @@
 #! /bin/bash
 
-#script to auto-install ns-3.  assumes that boost is built by source and installed to the home directory under ~/boost/include and ~/lib
+#script to auto-install ns-3.  *NO LONGER* assumes that boost is built by source and installed to the home directory under ~/boost/include and ~/lib
 
-cd /scratch/kebenson
+#cd /scratch/kebenson
 wget 'http://www.nsnam.org/releases/latest'
 tar -xvf ns-allinone-3.*.tar.bz2
 cd ns-allinone-3.*
+cd ns-3.*
+
+#need to clone the repo
+#but into an empty directory
+mkdir .tempns3
+cd .tempns3
+
+git clone git@github.com:KyleBenson/ns3.git
+cd ns3
+git checkout geocron
+git reset --hard HEAD
+cd ..
+
+#move all the git updated ns3 files to the fresh release ns3 folder
+mv ns3/* ../..
+mv ns3/.git* ../.. #above doesn't get the hiddens
+rm -rf ns3
+cd .. #out of temp file
+rm -rf .tempns3
+
 ./build.py #this will fail, but we need to get the other parts built
 
 #some funny build configurations were required when I built boost from source
