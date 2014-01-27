0e96ba5d2dc5fdd62063bc769df9b1491c8debe0
rangaraj <ranga.raj@uci.edu>
2014-01-27 01:37:07 -0800

    Utility methods added for sorting vectors

---
+++ scratch/mdc/mdc-utilities.cc
@@ -0,0 +1,131 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: UCI
+ */
+#include "mdc-utilities.h"
+/*
+#include "ns3/double.h"
+#include "ns3/string.h"
+#include "ns3/pointer.h"
+#include "ns3/uinteger.h"
+#include "ns3/enum.h"
+*/
+#include "ns3/log.h"
+
+NS_LOG_COMPONENT_DEFINE ("MDCUtilities");
+
+namespace ns3 {
+
+
+	// Returns the position vector closest to the refPoint provided.
+	Vector GetClosestVector (std::vector<Vector> posVector, Vector refPoint)
+	{
+		Vector retV;
+		std::vector<Vector>::iterator it;
+		double d = INFINITY;
+		double temp = 0;
+		for (it = posVector.begin(); it != posVector.end();)
+		{
+			temp = CalculateDistance(*it, refPoint);
+			if (temp < d)
+			{
+				d = temp;
+				retV.x = it->x;
+				retV.y = it->y;
+				retV.z = it->z;
+			}
+			it++;
+		}
+		return retV;
+	}
+
+	bool IsSameVector (Vector *aV, Vector *bV)
+	{
+		if (aV == bV)
+			return true;
+		else
+			return false;
+
+		if ((fabs((aV->x - bV->x)) < EPSILON) &&
+			(fabs((aV->y - bV->y)) < EPSILON) &&
+			(fabs((aV->z - bV->z)) < EPSILON) )
+			return true;
+		else
+			return false;
+	}
+
+	std::queue<unsigned> NearestNeighborOrder (std::vector<Vector> *inputVector, Vector refPoint)
+	{
+		std::queue<unsigned> orderSeq;
+		std::set<unsigned> visitedSet;
+		unsigned vectorSize = (*inputVector).size();
+
+		double minDistance;
+		Vector newRefPos; // Keeping track of the current ref point
+		unsigned currMinIndex; // Keeping track of min node in the curr iteration
+
+		newRefPos = refPoint;
+//		std::vector<Vector>::iterator it;
+		std::set<unsigned>::iterator it;
+
+		for (unsigned j=0; j<vectorSize; j++)
+		{
+			minDistance = INFINITY;
+			currMinIndex = 0;
+			for (unsigned i=0; i<vectorSize; i++)
+			{
+				if (visitedSet.find(i) != visitedSet.end() )
+				{
+					// Node is already visited.
+					;
+				}
+				else
+				{
+					double temp = CalculateDistance((*inputVector)[i], newRefPos);
+					if (temp < minDistance)
+					{
+						currMinIndex = i;
+						minDistance = temp;
+					}
+				}
+			}
+			orderSeq.push(currMinIndex);
+			visitedSet.insert(currMinIndex);
+			newRefPos = (*inputVector)[currMinIndex];
+//			std::cout << "Next Nearest Sensor = " << newRefPos.x << "," << newRefPos.y << "," << newRefPos.z << "].\n";
+		}
+		return orderSeq;
+	}
+
+	std::vector<Vector> ReSortInputVector (std::vector<Vector> *inputVector, std::queue<unsigned> sortSeq)
+	{
+		std::vector<Vector> outputVector;
+		unsigned u;
+		while (!sortSeq.empty())
+		{
+			u = sortSeq.front();
+			outputVector.push_back((*inputVector)[u]);
+			sortSeq.pop();
+//			std::cout << "Sorted Sensor Position = [" << (*inputVector)[u].x << "," << (*inputVector)[u].y << "," << (*inputVector)[u].z << "].\n";
+		}
+
+		return outputVector;
+	}
+
+
+} // namespace ns3 
---
+++ scratch/mdc/mdc-utilities.h
@@ -0,0 +1,42 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: UCI
+ */
+
+#ifndef MDC_UTILITIES_H
+#define MDC_UTILITIES_H
+
+#include "ns3/object.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/vector.h"
+#include <math.h>
+#include <set>
+#include <queue>
+
+#define EPSILON 0.0001
+
+namespace ns3 {
+
+	Vector GetClosestVector (std::vector<Vector> posVector, Vector refPoint);
+	bool IsSameVector (Vector *aV, Vector *bV);
+	std::queue<unsigned> NearestNeighborOrder (std::vector<Vector> *inputVector, Vector refPoint);
+	std::vector<Vector> ReSortInputVector (std::vector<Vector> *inputVector, std::queue<unsigned> sortSeq);
+
+} // namespace ns3
+
+#endif /* MDC_POSITION_H */
