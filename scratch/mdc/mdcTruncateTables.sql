e80ad473e32fe8c8ab9ab3c1d002e9c70b79d7b5
rangaraj <ranga.raj@uci.edu>
2014-02-18 22:55:36 -0800

    Script to clean out the database if required

---
+++ scratch/mdc/mdcTruncateTables.sql
@@ -0,0 +1,9 @@
+use pc3;
+truncate table COLL_TRACE;
+truncate table CREATED_EVENTS  ;
+truncate table DETECTED_EVENTS ;
+truncate table EVENT_DELAY     ;
+truncate table PACKET_DROP     ;
+truncate table SENSOR_TRACE    ;
+truncate table SINK_TRACE      ;
+
